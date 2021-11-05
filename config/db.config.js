04ad4f53ab328535e481c893f45cbbbbf1d2ef1c
ShuntBusiness <84186486+ShuntBusiness@users.noreply.github.com>
2021-11-05 11:59:32 -0300

    Add files via upload

--- config/db.config.js
+++ config/db.config.js
@@ -1,5 +1,6 @@
 'use strict';
-const mysql = require('mysql');
+import mysql from 'mysql';
+import module from 'module';
 //local mysql db connection
 const dbConn = mysql.createConnection({
   host     : '54.166.223.8',
@@ -12,3 +13,4 @@ dbConn.connect(function(err) {
   console.log("Database Connected!");
 });
 module.exports = dbConn;
+export default dbConn;
