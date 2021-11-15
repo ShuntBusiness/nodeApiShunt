6e6c5c36313ca113629c0dd4c7a2613ed0b1c18a
ShuntBusiness <84186486+ShuntBusiness@users.noreply.github.com>
2021-11-15 18:27:22 -0300

    Update razor.model.js

--- models/razor.model.js
+++ models/razor.model.js
@@ -19,7 +19,7 @@ razor.razeUrl = function (url, result) {
   console.log(headers);
   var urlencoded = new URLSearchParams();
   urlencoded.append("extractors", "entities");
-  urlencoded.append("url", "http://" + url);
+  urlencoded.append("url", "https://" + url);
 
 
   var requestOptions = {
@@ -30,7 +30,7 @@ razor.razeUrl = function (url, result) {
   };
 
 
-  fetch("http://api.textrazor.com?extractors=entities&url=http://" + url, requestOptions).then(response => response.text())
+  fetch("https://api.textrazor.com?extractors=entities&url=https://" + url, requestOptions).then(response => response.text())
   .then(response => result(null, response));
 
 };
