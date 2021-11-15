43a3d405cf06e365d38cb4c28e16160d7708a1ef
ShuntBusiness <84186486+ShuntBusiness@users.noreply.github.com>
2021-11-15 17:51:47 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ import peliRoutes from './routes/pelis.routes.js';
 import juegoRoutes from './routes/juegos.routes.js';
 import serieRoutes from './routes/series.routes.js';
 import razorRoutes from './routes/razor.routes.js';
+import https from 'https';
+import fs from 'fs';
 // create express app
 const app = express();
 // Setup server port
@@ -23,6 +25,13 @@ app.use('/api/v1/juegos', juegoRoutes);
 app.use('/api/v1/series', serieRoutes);
 app.use('/razor/v1/', razorRoutes);
 // listen for requests
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-});
+//app.listen(port, () => {
+  //console.log(`Server is listening on port ${port}`);
+//});
+
+var options = {
+  key: fs.readFileSync('client-key.pem'),
+  cert: fs.readFileSync('client-cert.cert')
+};
+
+https.createServer(options, app).listen(443);
