621380a736f3c7c3992d8d70cf589d01a7d60978
ShuntBusiness <84186486+ShuntBusiness@users.noreply.github.com>
2021-11-25 17:43:50 -0300

    Update pelis.routes.js

--- routes/pelis.routes.js
+++ routes/pelis.routes.js
@@ -2,18 +2,27 @@ import express from 'express';
 const router = express.Router();
 import peliController from '../controller/pelis.controller.js';
 import module from 'module';
+
 // Retrieve all pelis
 router.get('/obtenerpelis', peliController.findAll);
+
+// Retrieve all personajes
+router.get("/obtenerpersonajes", peliController.getAllCharacters);
+
 // Create a new peli
 router.post('/nuevapeli', peliController.create);
+
 // Retrieve a single peli with id
 router.get('/buscarpeli/:id', peliController.findById);
 
 // Retrieve keywords with id
 router.get('/obtenerkeywords/:id', peliController.getKeyWords);
+
 // Update a peli with id
 router.put('/actualizarpeli/:id', peliController.update);
+
 // Delete a peli with id
 router.delete('/borrarpeli/:id', peliController.delete);
+
 module.exports = router
 export default router
