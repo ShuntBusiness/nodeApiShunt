bb2a6c8c6312a6babaef8cd476fbe93116801bba
ShuntBusiness <84186486+ShuntBusiness@users.noreply.github.com>
2021-11-25 17:51:28 -0300

    Update razor.controller.js

--- controller/razor.controller.js
+++ controller/razor.controller.js
@@ -4,6 +4,7 @@ import exports from 'module';
 
 exports.razeUrl = function(req, res) {
   razor.razeUrl(req.query.url, function(err, razor) {
+    res.append('Access-Control-Allow-Origin', '*');
     if (err) res.send(err);
     res.send(razor);
   });
